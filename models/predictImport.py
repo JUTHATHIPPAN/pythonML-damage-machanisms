d7187a9ec95417cb4cd02fff0b574e5a526562f4
JUTHATHIPPAN <juthathip.panyapunyarat@dexon-technology.com>
2023-07-17 16:52:55 +0700

    import excel file for predict

--- models/predictImport.py
+++ models/predictImport.py
@@ -1,38 +1,56 @@
 from pydantic import BaseModel
 from fastapi import UploadFile, File
 from typing import Optional
+from datetime import date
 
 class FileUpload(BaseModel):
+    projectID: int
     files: bytes
+    
+class projectListRes(BaseModel):
+    id: Optional[str] = None
+    project_id: Optional[str] = None
+    project_name: Optional[str] = None
+    remark: Optional[str] = None
+    added_date: Optional[date] = None
+    updated_date: Optional[date] = None
 
-class DamagePrediction(BaseModel):
-    material: Optional[int] = None
-    operatingPressure: Optional[float] = None
+class equipmentReq(BaseModel):
+    project_id: Optional[int] = None
+    
+class equipmentRes(BaseModel):
+    id: Optional[int] = None #id of tb_equipment
+    equipment_id: Optional[str] = None #name of equipment
+    equipment_info_id: Optional[int] = None #id of tb_equipment_info
+    materialGrade_id: Optional[float] = None
+    materialGrade: Optional[str] = None
     operatingTemperature: Optional[float] = None
-    designPressure: Optional[float] = None
     designTemperature: Optional[float] = None
-    insulation: Optional[int] = None
+    operatingPressure: Optional[float] = None
+    designPressure: Optional[float] = None
+    insulation: Optional[str] = None
     amineContains: Optional[float] = None
-    postWeldHeatTreatment: Optional[int] = None
+    postWeldHeatTreatment: Optional[str] = None
     hydrogenContains: Optional[float] = None
     ultimateTensileStrength: Optional[float] = None
     operatingHydrogenPartialPressure: Optional[float] = None
     h2sContains: Optional[float] = None
     TAN: Optional[float] = None
-    fluidPhase: Optional[int] = None
+    fluidPhase: Optional[str] = None
     sulfurContains: Optional[float] = None
-    modelFluid: Optional[int] = None
-    exposureToErosion: Optional[int] = None
-    injectionPoint: Optional[int] = None
-    solidParticle_droplets: Optional[int] = None
+    modelFluid: Optional[str] = None
+    exposureToErosion: Optional[str] = None
+    injectionPoint: Optional[str] = None
+    solidParticle_droplets: Optional[str] = None
     MDMT: Optional[float] = None
-    MDMT_MAT: Optional[int] = None
+    MDMT_MAT: Optional[str] = None
     waterContains: Optional[float] = None
-    externalEnvironment: Optional[int] = None
+    externalEnvironment: Optional[str] = None
     pH: Optional[float] = None
-    junctionOfDissimilarMetals: Optional[int] = None
-    nickel_basedAlloy: Optional[int] = None
+    junctionOfDissimilarMetals: Optional[str] = None
+    nickel_basedAlloy: Optional[str] = None
     oxygenExist: Optional[float] = None
     MBHW: Optional[float] = None
-    exposeToSoil: Optional[int] = None
-    
+    exposeToSoil: Optional[str] = None
+    added_date:Optional[date] = None
+    updated_date:Optional[date] = None
